7a055badc00c966cda49c7f0daaca6585e16b204
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-24 11:06:55 -0500

    Created POST /users route

--- Sources/App/Configuration/routes.swift
+++ Sources/App/Configuration/routes.swift
@@ -5,4 +5,8 @@ public func routes(_ router: Router) throws {
     router.get { (request) in
         return "Hello Vapor!"
     }
+    
+    router.post(User.self, at: "users") { (request, user) in
+        return user.save(on: request)
+    }
 }
