1f80344b06e00efd17972635aa5b3d199f2cc14f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-07 18:31:06 -0500

    Created UserController.create route handler

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -4,6 +4,10 @@ import Fluent
 final class UserController: RouteCollection {
     func boot(router: Router) throws {}
     
+    func create(_ request: Request, _ user: User)throws -> Future<User> {
+        return user.save(on: request)
+    }
+    
     func index(_ request: Request)throws -> Future<[User]> {
         return User.query(on: request).all()
     }
