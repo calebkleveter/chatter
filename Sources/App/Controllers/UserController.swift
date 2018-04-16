89ecbc445154db11241d9368b94d3ecad04e2abb
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-04-16 17:01:20 -0500

    Created UserController.update route handler

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -16,7 +16,27 @@ final class UserController: RouteCollection {
         return try request.parameter(User.self)
     }
     
+    func update(_ request: Request, _ body: UserContent)throws -> Future<User> {
+        let user = try request.parameter(User.self)
+        return user.map(to: User.self, { user in
+            user.username = body.username ?? user.username
+            user.firstname = body.firstname ?? user.firstname
+            user.lastname = body.lastname ?? user.lastname
+            user.email = body.email ?? user.email
+            user.password = body.password ?? user.password
+            return user
+        }).update(on: request)
+    }
+    
     func delete(_ request: Request)throws -> Future<HTTPStatus> {
         return try request.parameter(User.self).transform(to: .noContent)
     }
+}
+
+struct UserContent: Content {
+    var username: String?
+    var firstname: String?
+    var lastname: String?
+    var email: String?
+    var password: String?
 }
