5d2394e6135c987b467251309e89d1535d86557b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-05-05 17:28:21 -0500

    Registered UserController with the main router

--- Sources/App/Configuration/routes.swift
+++ Sources/App/Configuration/routes.swift
@@ -2,11 +2,5 @@ import Routing
 import Vapor
 
 public func routes(_ router: Router) throws {
-    router.get { (request) in
-        return "Hello Vapor!"
-    }
-    
-    router.post(User.self, at: "users") { (request, user) in
-        return user.save(on: request)
-    }
+    try router.register(collection: UserController())
 }
