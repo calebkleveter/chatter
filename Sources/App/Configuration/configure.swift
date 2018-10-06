8a14ece9da85fda6eb351c0ec52643e393337577
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-06 16:33:33 -0500

    Added Tag model to migration config

--- Sources/App/Configuration/configure.swift
+++ Sources/App/Configuration/configure.swift
@@ -25,6 +25,7 @@ public func configure(
     migrations.add(model: User.self, database: .psql)
     migrations.add(model: UserConnection.self, database: .psql)
     migrations.add(model: Post.self, database: .psql)
+    migrations.add(model: Tag.self, database: .psql)
     services.register(migrations)
     
     var commands = CommandConfig.default()
