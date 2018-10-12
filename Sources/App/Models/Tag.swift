5bc6b91d2ccc287f62c5ac568066b2ca1a41e6ee
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-12 11:22:00 -0500

    Fetch stored Tag from database if attmpted insert fails with duplicate 'name' property

--- Sources/App/Models/Tag.swift
+++ Sources/App/Models/Tag.swift
@@ -8,6 +8,18 @@ final class Tag {
     init(name: String) {
         self.name = name
     }
+    
+    func save(on conn: DatabaseConnectable) -> EventLoopFuture<Tag> {
+        return Tag.query(on: conn).save(self).catchFlatMap { error in
+            guard let psqlError = error as? PostgreSQLError else { throw error }
+            if psqlError.identifier == "server.error._bt_check_unique" {
+                let error = NotFound(rootCause: FluentError(identifier: "modelNotFound", reason: "No tag with name '\(self.name)' was found"))
+                return Tag.query(on: conn).filter(\.name == self.name).first().unwrap(or: error)
+            } else {
+                throw error
+            }
+        }
+    }
 }
 
 extension Tag: Migration {
