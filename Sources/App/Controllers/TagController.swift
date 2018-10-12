08370319caabb8c195a35f859bc318d7c9fc36c0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-12 12:47:31 -0500

    Created TagController.posts route

--- Sources/App/Controllers/TagController.swift
+++ Sources/App/Controllers/TagController.swift
@@ -3,6 +3,14 @@ import Vapor
 
 final class TagController: RouteCollection {
     func boot(router: Router) throws {
+        let tags = router.grouped("tags", Tag.parameter)
         
+        tags.get("posts", use: posts)
+    }
+    
+    func posts(_ request: Request)throws -> Future<[Post]> {
+        return try request.parameters.next(Tag.self).flatMap { tag in
+            return try tag.posts.query(on: request).all()
+        }
     }
 }
