919dbc3f36d9637f9d4ca1c424cb80284eb0878c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-12 12:45:35 -0500

    Created TagController route collection

--- Sources/App/Configuration/routes.swift
+++ Sources/App/Configuration/routes.swift
@@ -4,4 +4,5 @@ import Vapor
 public func routes(_ router: Router) throws {
     try router.register(collection: UserController())
     try router.register(collection: PostController())
+    try router.register(collection: TagController())
 }
---
+++ Sources/App/Controllers/TagController.swift
@@ -0,0 +1,8 @@
+import FluentPostgreSQL
+import Vapor
+
+final class TagController: RouteCollection {
+    func boot(router: Router) throws {
+        
+    }
+}
